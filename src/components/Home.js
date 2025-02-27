608970fc4aecd026c7b88e7c337c615b4c3feea4
Daryan Chan <daryan.j.chan@hotmail.com>
2025-02-27 15:07:17 -0500

    initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import BlogList from "./BlogList";
 function Home() {
   const [name, setName] = useState("Mario");
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   const handleClick = () => {
     setName("Luigi");
@@ -12,19 +13,24 @@ function Home() {
     console.log("Hello " + name);
   };
 
+  //using useEffect to simulate loading data and then remove <div>Loading...</div> once data has been fetched
   useEffect(function () {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-      });
+    setTimeout(function () {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
       {blogs && (
         <BlogList

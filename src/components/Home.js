e3c46093c08166f663c5cf791f45416e00119fe0
Daryan Chan <daryan.j.chan@hotmail.com>
2025-02-27 14:55:29 -0500

    lesson 17 commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 function Home() {
   const [name, setName] = useState("Mario");
-  const [blogs, setBlogs] = useState([
-    {
-      title: "New new website",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 1,
-    },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "luigi", id: 2 },
-    { title: "React!", body: "lorem ipsum...", author: "yoshi", id: 3 },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const handleClick = () => {
     setName("Luigi");
@@ -19,17 +11,46 @@ function Home() {
   const handleClickAgain = (name) => {
     console.log("Hello " + name);
   };
+
+  function handleDelete(id) {
+    const newBlogs = blogs.filter((blog) => {
+      return blog.id != id;
+    });
+    setBlogs(newBlogs);
+  }
+
+  useEffect(function () {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
+
   return (
     <div className="home">
-      {blogs.map((blog) => {
-        return (
-          <div className="blog-preview" key={blog.id}>
-            <h2>{blog.title}</h2>
-            <p>Written by {blog.author}</p>
-          </div>
-        );
-      })}
-
+      {blogs && (
+        <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      )}
+      {blogs && (
+        <BlogList
+          blogs={blogs.filter((blog) => {
+            return blog.author === "mario";
+          })}
+          title="Mario's Blogs"
+          handleDelete={handleDelete}
+        />
+      )}
+      <button
+        onClick={() => {
+          setName("Tommy");
+        }}
+      >
+        Change Name
+      </button>
       <p>{name}</p>
       <button onClick={handleClick}>Click Me</button>
       <button

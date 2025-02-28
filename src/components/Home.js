db542794d25968c06207b32f7ddc49e33e44aa17
Daryan Chan <daryan.j.chan@hotmail.com>
2025-02-28 15:16:51 -0500

    configured error handling

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ function Home() {
   const [name, setName] = useState("Mario");
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleClick = () => {
     setName("Luigi");
@@ -18,18 +19,27 @@ function Home() {
     setTimeout(function () {
       fetch("http://localhost:8000/blogs")
         .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not fetch the data for that resource");
+          }
           return res.json();
         })
         .then((data) => {
           console.log(data);
           setBlogs(data);
           setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
         });
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
       {blogs && (

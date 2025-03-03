2e4ea83186d333ddfd692ed1051464361ab98cec
Daryan Chan <daryan.j.chan@hotmail.com>
2025-03-02 23:14:24 -0500

    exact route routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Create from "./components/Create";
 import {
   BrowserRouter as Router,
   Route,
@@ -17,6 +18,7 @@ function App() {
         <div className="content">
           <Routes>
             <Route exact path="/" element={<Home />} />
+            <Route exact path="/create" element={<Create />} />
           </Routes>
         </div>
       </div>

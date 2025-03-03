68c3089377bb9344768044d836e07024657994ff
Daryan Chan <daryan.j.chan@hotmail.com>
2025-03-03 15:19:15 -0500

    Route Parameters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Create from "./components/Create";
+import BlogDetails from "./components/BlogDetails";
 import {
   BrowserRouter as Router,
   Route,
@@ -19,6 +20,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/create" element={<Create />} />
+            <Route exact path="/blogs/:id" element={<BlogDetails />} />
           </Routes>
         </div>
       </div>
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function BlogList({ blogs, title }) {
   //   const blogs = props.blogs;
@@ -10,9 +11,10 @@ function BlogList({ blogs, title }) {
       {blogs.map((blog) => {
         return (
           <div className="blog-preview" key={blog.id}>
-            <h2>{blog.title}</h2>
-            <p>Written by {blog.author}</p>
-
+            <Link to={`/blogs/${blog.id}`}>
+              <h2>{blog.title}</h2>
+              <p>Written by {blog.author}</p>
+            </Link>
           </div>
         );
       })}

ba979af3d948cceb8f30465273ca492ca2229c78
Daryan Chan <daryan.j.chan@hotmail.com>
2025-03-02 21:13:21 -0500

    adding react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+} from "react-router-dom";
 
 function App() {
   const title = "Welcome to the new blog";
   const likes = 50;
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 

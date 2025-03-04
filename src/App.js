3a8fdb0a62233c3e49e1e8f6674b8f4d01352ad6
Daryan Chan <daryan.j.chan@hotmail.com>
2025-03-04 18:49:25 -0500

    404 Pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Create from "./components/Create";
 import BlogDetails from "./components/BlogDetails";
+import NotFound from "./components/NotFound";
 import {
   BrowserRouter as Router,
   Route,
@@ -21,6 +22,7 @@ function App() {
             <Route exact path="/" element={<Home />} />
             <Route exact path="/create" element={<Create />} />
             <Route exact path="/blogs/:id" element={<BlogDetails />} />
+            <Route expact path="*" element={<NotFound />}></Route>
           </Routes>
         </div>
       </div>
